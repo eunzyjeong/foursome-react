7996991ccaadfb78f6033a016618fe9e75d85d0e
Juun Pak <june.mintlatte@gmail.com>
2021-12-12 08:57:39 +0900

    📝 Create data related to Works component

---
+++ src/data/Work.js
@@ -0,0 +1,38 @@
+import img_1 from 'img/work_img.png';
+import detail_img1 from 'img/work_detail.png';
+
+const Work = [
+  {
+    id: 1,
+    title: '보이지 않는 경계',
+    client: '옐토',
+    category: 'Exhibition',
+    published_year: 2021,
+    description:
+      '신촌문화관에서 열린 <보이지 않는 경계선 : 지우개>展에 참여했습니다. 포썸은 전시시공 및 배너, 어쩌고 저쩌고를 담당했습니다.',
+    img: `${img_1}`,
+    detail_img: [`${detail_img1}`, `${detail_img1}`, `${detail_img1}`]
+  },
+  {
+    id: 2,
+    title: '보이지 않는 경계',
+    client: '옐토',
+    category: 'Exhibition',
+    published_year: 2021,
+    description:
+      '신촌문화관에서 열린 <보이지 않는 경계선 : 지우개>展에 참여했습니다. 포썸은 전시시공 및 배너, 어쩌고 저쩌고를 담당했습니다.',
+    img: `${img_1}`
+  },
+  {
+    id: 3,
+    title: '보이지 않는 경계',
+    client: '옐토',
+    category: 'Exhibition',
+    published_year: 2021,
+    description:
+      '신촌문화관에서 열린 <보이지 않는 경계선 : 지우개>展에 참여했습니다. 포썸은 전시시공 및 배너, 어쩌고 저쩌고를 담당했습니다.',
+    img: `${img_1}`
+  }
+];
+
+export default Work;
