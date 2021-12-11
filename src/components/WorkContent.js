0b0c12b1e253dabfb7fa3deaf1008ec704ac65b3
Juun Pak <june.mintlatte@gmail.com>
2021-12-12 08:58:23 +0900

    ✨ Makeup WorkContents component

--- src/components/WorkContent.js
+++ src/components/WorkContent.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import 'styles/WorkContent.scss';
 
 const WorkContents = ({ worksData }) => {
   return (
-    <div>
-      {worksData.map(content => (
-        <React.Fragment key={content.id}>
-          <h1>{content.title}</h1>
-          <ul>
-            <li>{content.author}</li>
-            <li>{content.category}</li>
-            <li>{content.published_year}</li>
-          </ul>
-          <img src={content.img} alt={content.title} />
-        </React.Fragment>
-      ))}
+    <div className="WorkContent">
+      <div className="work-content">
+        {worksData.map(content => (
+          <React.Fragment key={content.id}>
+            <Link to={`/works/${content.id}`}>
+              <div className="work-content__title">
+                <h1>{content.title}</h1>
+              </div>
+            </Link>
+            <ul className="work-content__list">
+              <li className="work-content__item">{content.client}</li>
+              <li className="work-content__item">{content.category}</li>
+              <li className="work-content__item">{content.published_year}</li>
+            </ul>
+            <div className="work-content__img">
+              <img src={content.img} alt={content.title} />
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 };
