2e99d27d6e03c356745566887f67072075d8696b
Juun Pak <june.mintlatte@gmail.com>
2021-12-11 21:02:48 +0900

    ✨ Makeup Foursome logo

---
+++ src/components/Logo.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Logo = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="36"
+    height="36"
+    viewBox="0 0 36 36"
+  >
+    <g id="그룹_1" data-name="그룹 1" transform="translate(0 271.477)">
+      <path
+        id="패스_1"
+        data-name="패스 1"
+        d="M52.016-271.477h-18v18h9v-9h9Z"
+        transform="translate(-16.016)"
+        fill="#FFE500"
+        style={{ mixBlendMode: 'multiply;isolation: isolate' }}
+      />
+      <path
+        id="패스_2"
+        data-name="패스 2"
+        d="M18-271.477H0v18H9v-9h9Z"
+        fill="#009FE3"
+        style={{ mixBlendMode: 'multiply;isolation: isolate' }}
+      />
+      <path
+        id="패스_3"
+        data-name="패스 3"
+        d="M18-237.461H0v18H9v-9h9Z"
+        transform="translate(0 -16.016)"
+        fill="#d8006b"
+        style={{ mixBlendMode: 'multiply;isolation: isolate' }}
+      />
+      <path
+        id="패스_4"
+        data-name="패스 4"
+        d="M34.016-219.461h18v-18h-9v9h-9Z"
+        transform="translate(-16.016 -16.016)"
+        style={{ mixBlendMode: 'multiply;isolation: isolate' }}
+      />
+    </g>
+  </svg>
+);
+
+export default Logo;
