bd51457c89005bf2a7f36c640948094d1bc273d1
Juun Pak <june.mintlatte@gmail.com>
2021-12-12 10:45:43 +0900

    ✨ Makeup Blog component

---
+++ src/routes/Blog.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navigation from 'components/Navigation';
+import BlogData from 'data/Blog';
+
+import 'styles/Blog.scss';
+import blue from 'img/blue_box.png';
+import yellow from 'img/yellow_box.png';
+import pink from 'img/pink_box.png';
+
+const Blog = () => {
+  const navList = [
+    'All',
+    'Foursome',
+    'News',
+    'VMD',
+    'Printing&press',
+    'Material',
+    'Coating',
+    'Ink'
+  ];
+
+  return (
+    <div className="Blog">
+      <Navigation navList={navList} currentPath="/blog" />
+      <div className="blog-list">
+        {BlogData.map(content => (
+          <Link to={`/blog/${content.id}`}>
+            <div className="blog-content" key={content.id}>
+              <div className="blog-content__img">
+                <img src={content.thumbnail} alt={content.title} />
+              </div>
+              <div className="blog-content__text">
+                <h3 className="blog-content__title">{content.title}</h3>
+                <span className="blog-content__category">
+                  {content.catogory}
+                </span>
+              </div>
+            </div>
+          </Link>
+        ))}
+        <div className="blog-colorbox">
+          <img src={blue} alt="파란색" />
+        </div>
+        <div className="blog-colorbox">
+          <img src={yellow} alt="노란색" />
+        </div>
+        <div className="blog-colorbox">
+          <img src={pink} alt="핑크색" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
