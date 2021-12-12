09cb357274a6ad1f4b0b20a4923b6eb64969a0c2
Juun Pak <june.mintlatte@gmail.com>
2021-12-12 10:45:15 +0900

    📝 Create blog sample data

---
+++ src/data/Blog.js
@@ -0,0 +1,26 @@
+import thumbnail_1 from 'img/blog1.png';
+import thumbnail_2 from 'img/blog2.png';
+import thumbnail_3 from 'img/blog3.png';
+
+const Blog = [
+  {
+    id: 1,
+    title: '어디든 자유롭게 꾸며봐',
+    catogory: '스티커',
+    thumbnail: thumbnail_1
+  },
+  {
+    id: 2,
+    title: '우리 공간 앞 마케팅 채널',
+    catogory: '배너',
+    thumbnail: thumbnail_2
+  },
+  {
+    id: 3,
+    title: '전시공간의 필수품',
+    catogory: '현수막',
+    thumbnail: thumbnail_3
+  }
+];
+
+export default Blog;
