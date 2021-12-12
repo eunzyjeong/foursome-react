962df5890f6dfa53f2d787096bb614dfc7aba6ea
Juun Pak <june.mintlatte@gmail.com>
2021-12-12 10:44:02 +0900

    ⚡️ Change color to links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import 'styles/Navigation.scss';
 
 const Navigation = ({ navList, currentPath }) => {
+  const { pathname } = useLocation();
+
   return (
     <nav>
       <ul className="category">
         {navList.map((item, index) => (
           <li key={index} className="category__list">
-            <Link to="/works">{item}</Link>
+            <Link
+              to={currentPath}
+              className={`category__item ${
+                pathname.includes('/works')
+                  ? 'blue'
+                  : pathname.includes('/blog')
+                  ? 'pink'
+                  : ''
+              }`}
+            >
+              {item}
+            </Link>
           </li>
         ))}
       </ul>
