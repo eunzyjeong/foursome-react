c8222f89289a9ea638aac4539805d6e4653079b4
Juun Pak <june.mintlatte@gmail.com>
2021-12-12 10:41:47 +0900

    👔 Update route

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,10 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from 'components/Header';
 import About from 'routes/About';
-import Blog from 'routes/Blog';
 import Works from 'routes/Works';
 import WorkDetail from 'routes/WorkDetail';
+import Blog from 'routes/Blog';
+import BlogPost from 'routes/BlogPost';
 
 const Router = () => (
   <HashRouter>
@@ -13,6 +14,7 @@ const Router = () => (
       <Route path="/works" element={<Works />} />
       <Route path="/works/:detailId" element={<WorkDetail />} />
       <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/:postId" element={<BlogPost />} />
     </Routes>
   </HashRouter>
 );
