13d0aad6fc664c7d19df33d965a888ad4b044cd1
Juun Pak <june.mintlatte@gmail.com>
2021-12-12 11:04:34 +0900

    ✨ Makeup BlogPost components

---
+++ src/routes/BlogPost.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Navigation from 'components/Navigation';
+
+import 'styles/BlogPost.scss';
+import postImg_1 from 'img/blog_post.png';
+
+const BlogPost = () => {
+  const navList = [
+    'All',
+    'Foursome',
+    'News',
+    'VMD',
+    'Printing&press',
+    'Material',
+    'Coating',
+    'Ink'
+  ];
+
+  return (
+    <div className="BlogPost">
+      <Navigation navList={navList} currentPath="/blog" />
+      <div className="post">
+        <h1 className="post__title">어디든 자유롭게 꾸며봐</h1>
+        <div className="post-contents">
+          <div className="post-contents__img">
+            <img src={postImg_1} alt="포스팅 이미지" />
+          </div>
+          <p className="post-contents__text">
+            안녕하세요 오늘은 어쩌고 저쩌고. 대충 조각스티커랑 판스티커가 왜
+            다른지 뭐가 다른지 리무버블스티커, 종이스티커, 백릿스티커, 배면
+            인쇄(양면)스티커 등등 알려드릴거에요!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
