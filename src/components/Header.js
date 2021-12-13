291443ef78ce9dcc886bdf0b35a323c1eaa62218
Juun Pak <june.mintlatte@gmail.com>
2021-12-13 14:50:34 +0900

    ♻️ Organize the codes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
-import Logo from 'components/Logo';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import Logo from 'components/Logo';
+
 import 'styles/Header.scss';
 
 const Header = () => {
