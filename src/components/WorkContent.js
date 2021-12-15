24e090ff13df42806a4760bab4c9ff076704de50
Juun Pak <june.mintlatte@gmail.com>
2021-12-15 09:06:37 +0900

    💄 Set link area

--- src/components/WorkContent.js
+++ src/components/WorkContent.js
@@ -6,26 +6,30 @@ import 'styles/WorkContent.scss';
 const WorkContents = ({ worksData }) => {
   return (
     <div className="scroll-area">
-      <section className="WorkContent">
-        <article className="work-content">
+      <section className="WorkContents">
+        <article className="work-contents">
           {worksData.map(content => (
             <React.Fragment key={content.id}>
-              <Link to={`/works/${content.id}`}>
-                <header className="work-content__title">
-                  <div className="marquee-overflow">
-                    {content.title_array.map((title, index) => (
-                      <h1 key={index}>{title}</h1>
-                    ))}
+              <div className="work-content">
+                <Link to={`/works/${content.id}`}>
+                  <header className="work-content__title">
+                    <div className="marquee-overflow">
+                      {content.title_array.map((title, index) => (
+                        <h1 key={index}>{title}</h1>
+                      ))}
+                    </div>
+                  </header>
+                  <ul className="work-content__list">
+                    <li className="work-content__item">{content.client}</li>
+                    <li className="work-content__item">{content.category}</li>
+                    <li className="work-content__item">
+                      {content.published_year}
+                    </li>
+                  </ul>
+                  <div className="work-content__img">
+                    <img src={content.img} alt={content.title} />
                   </div>
-                </header>
-              </Link>
-              <ul className="work-content__list">
-                <li className="work-content__item">{content.client}</li>
-                <li className="work-content__item">{content.category}</li>
-                <li className="work-content__item">{content.published_year}</li>
-              </ul>
-              <div className="work-content__img">
-                <img src={content.img} alt={content.title} />
+                </Link>
               </div>
             </React.Fragment>
           ))}
