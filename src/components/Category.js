e0a635f2226e56c91f388ba538b49030e597d33f
Juun Pak <june.mintlatte@gmail.com>
2021-12-16 14:04:08 +0900

    📱 Set category component at mobile style

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import 'styles/components/Category.scss';
+import dropdown from 'img/btn_dropdown.png';
 
 const Category = ({ navList, currentPath }) => {
   const { pathname } = useLocation();
+  const [show, setShow] = useState(true);
+  const nav = useRef(null);
+
+  const onDropdownClick = () => {
+    setShow(prev => !prev);
+
+    if (show) nav.current.style.display = 'block';
+    else nav.current.style.display = 'none';
+  };
 
   return (
-    <nav>
-      <ul className="category">
-        <li className="category__mobile">All</li>
-        {navList.map((item, index) => (
-          <li key={index} className="category__list">
-            <Link
-              to={currentPath}
-              className={`category__item ${
-                pathname.includes('/works')
-                  ? 'blue'
-                  : pathname.includes('/blog')
-                  ? 'pink'
-                  : ''
-              }`}
-            >
-              {item}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <div className="category__mobile">
+        All
+        <div className="dropdown" onClick={onDropdownClick}>
+          <img src={dropdown} alt="드롭다운 버튼" />
+        </div>
+      </div>
+      <nav ref={nav}>
+        <ul className="category">
+          {navList.map((item, index) => (
+            <li key={index} className="category__list">
+              <Link
+                to={currentPath}
+                className={`category__item ${
+                  pathname.includes('/works')
+                    ? 'blue'
+                    : pathname.includes('/blog')
+                    ? 'pink'
+                    : ''
+                }`}
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 };
 
